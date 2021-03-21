2c1214b36e5f088b30fc99b9741e159ed6e767d6
AliMJ2021 <bo7ussain12@gmail.com>
2021-03-21 21:17:37 +0300

    update

--- hotel-booking/src/Components/slideshow.js
+++ hotel-booking/src/Components/slideshow.js
@@ -11,13 +11,13 @@ function Slideshow() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img className="d-block w-100" alt="First slide" />
+        <img className="d-block w-90" src='https://accommodation-med.com/wp-content/uploads/2016/08/Cheap-Hotels-Booking.jpg' alt="First slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" alt="Second slide" />
+        <img className="d-block w-90" src='https://images.unsplash.com/photo-1590073242678-70ee3fc28e8e?ixid=MXwxMjA3fDF8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60' alt="Second slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" alt="Third slide" />
+        <img className="d-block w-90" src='https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'alt="Third slide" />
       </Carousel.Item>
     </Carousel>
   );
